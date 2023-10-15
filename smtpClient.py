e8ad142beac6af6e0123734f65f04518fa738dd0
Shadman Kazi <141299353+shadmankazi9810@users.noreply.github.com>
2023-10-14 23:28:21 -0400

    Add files via upload

--- smtpClient.py
+++ smtpClient.py
@@ -47,9 +47,9 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send DATA command and handle server response.
     # Fill in start
-    dataCommand = 'DATA\r\n'
-    clientSocket.send(dataCommand.encode())
-    recv4 = clientSocket.recv(1024).decode()
+    #dataCommand = 'DATA\r\n'
+    #clientSocket.send(dataCommand.encode())
+    #recv4 = clientSocket.recv(1024).decode()
     #if recv4[:3] != '354':
         #print('354 reply not received from server.')
     # Fill in end

23e1347c39c9764d5f4992c856cea05098e21686
Shadman Kazi <141299353+shadmankazi9810@users.noreply.github.com>
2023-10-28 04:31:25 -0400

    Add files via upload

--- DNSClient.py
+++ DNSClient.py
@@ -2,11 +2,11 @@ import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
 local_host_ip = '127.0.0.1'
-real_name_server = '8.8.8.8' # Research public DNS servers to find a valid DNS server IP address to use
+real_name_server = '8.8.4.4' # Research public DNS servers to find a valid DNS server IP address to use
 
 
 # Create a list of domain names to query - use the same list from the DNS Server
-domainList  = ['example.com.','safebank.com','google.com','nyu.edu','legitsite.com']
+domainList  = ['example.com','safebank.com','google.com','nyu.edu','legitsite.com']
 
 # Define a function to query the local DNS server for the IP address of a given domain name
 def query_local_dns_server(domain, question_type):

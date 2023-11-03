ef39da6670bde9553646f10dd054469c7bec87ab
Shadman Kazi <141299353+shadmankazi9810@users.noreply.github.com>
2023-11-03 04:05:59 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 71136
+
+#problem 1
+p1Network = '32.109.214.48'
+p1Broadcast = '32.109.214.63'
+p1First = '32.109.214.49'
+p1Last =  '32.109.214.462'
+
+#problem 2
+p2Network = '199.94.176.0'
+p2Broadcast = '199.94.176.127'
+p2First = '199.94.176.1'
+p2Last = '199.94.176.126'
+
+#problem 3
+p3Network = '110.79.138.128'
+p3Broadcast = '110.79.138.135'
+p3First = '110.79.138.129'
+p3Last = '110.79.138.134'
+
+#problem 4
+p4Network = '21.64.100.128'
+p4Broadcast = '21.64.100.191'
+p4First = '21.64.100.129'
+p4Last = '21.64.100.190'
+
+#problem 5
+p5Network = '188.49.61.216'
+p5Broadcast = '188.49.61.223'
+p5First = '188.49.61.217'
+p5Last = '188.49.61.222'
+
+#problem 6
+p6Network = '99.34.23.0'
+p6Broadcast = '99.34.23.31'
+p6First = '	99.34.23.1'
+p6Last = '99.34.23.30'
+
+#problem 7
+p7Network = '10.19.241.32'
+p7Broadcast = '10.19.241.63'
+p7First = '10.19.241.33'
+p7Last = '10.19.241.62'
+
+#problem 8
+p8Network = '177.4.203.80'
+p8Broadcast = '177.4.203.95'
+p8First = '177.4.203.81'
+p8Last = '177.4.203.94'
+
+#problem 9
+p9Network = '88.246.164.128'
+p9Broadcast = '88.246.164.255'
+p9First = '88.246.164.129'
+p9Last = '88.246.164.254'
+
+#problem 10
+p10Network = '255.231.126.176'
+p10Broadcast = '255.231.126.183'
+p10First = '255.231.126.177'
+p10Last = '255.231.126.182'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
